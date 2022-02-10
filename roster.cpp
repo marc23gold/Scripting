63dc010528c0809eeca2588d57801042f9b36b51
Marcx <haitianhero6@gmail.com>
2022-02-10 16:48:57 -0500

    finished comments on to the rest of code

--- roster.cpp
+++ roster.cpp
@@ -7,6 +7,20 @@
 /*define the add function 
  that sets the Student class variables */
 
-/*
+/*define the remove function that removes students from the
+roster by studentID if the student ID does not exist, the function
+prints an error message indicating that the student was not found */
 
-//define the 
+/* define printALL function that prints a complete tab seperated list of student
+data in the provided format A1 [tab]
+the function should loop through all the students in classRosterArray and call the print()
+function for each student*/
+
+/* define printAverageDaysInCouse(string studentID) that correctly prints a student's average
+number of days in three courses. The student is indentified by the studentID parameter*/
+
+/*define printInvalidEmails() that verifies student email addresses and displays all invalid email
+addresses to the user*/
+
+/* define  printByDegreeProgram(DegreeProgram degreeProgram) that prints out student information for a degree program
+specified by an enumerated type */
